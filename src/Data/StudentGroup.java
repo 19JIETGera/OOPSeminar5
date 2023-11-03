7513784d043c0746adc9108f8edc17ff2e039ded
Gera <Elistratov.gera@yandex.ru>
2023-11-03 10:20:21 +0300

    Добавил Гетеры и Сетеры для StudentGroup.java и метод toString()

--- src/Data/StudentGroup.java
+++ src/Data/StudentGroup.java
@@ -1,2 +1,37 @@
-package Data;public class StudentGroup {
+package Data;
+
+import java.util.List;
+
+public class StudentGroup {
+    private List<Student> student;
+    private Teacher teacher;
+
+    public StudentGroup(List<Student> student, Teacher teacher) {
+        this.student = student;
+        this.teacher = teacher;
+    }
+
+    public List<Student> getStudent() {
+        return student;
+    }
+
+    public void setStudent(List<Student> student) {
+        this.student = student;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentGroup{" +
+                "students=" + student +
+                ", teacher=" + teacher +
+                '}';
+    }
 }
