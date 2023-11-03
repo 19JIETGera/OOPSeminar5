e03eb6f809471b8d02952f0f0aecc25b32d059fc
Gera <Elistratov.gera@yandex.ru>
2023-11-03 11:24:08 +0300

    Добавил метод создания учителя и вывод учителя в консоль

--- src/View/StudentView.java
+++ src/View/StudentView.java
@@ -1,9 +1,13 @@
 package View;
 
 import Data.Student;
+import Data.Teacher;
 
 public class StudentView {
     public void printOnConsole(Student student){
         System.out.println(student.toString());
+    }
+    public void printOnConsole(Teacher teacher){
+        System.out.println(teacher.toString());
     }
 }
